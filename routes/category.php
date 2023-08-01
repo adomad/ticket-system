719ddbb5f2479230ce87c3361829d2892c387c38
Ryan <ryanacharya98440@gmail.com>
2023-08-01 13:07:47 +0545

    add category routes

--- routes/category.php
+++ routes/category.php
@@ -7,3 +7,16 @@ use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/add_subcategory', [AdminController::class, 'add_subcategory'])->name('add_subcategory');
+Route::get('/add_category', [AdminController::class, 'category'])->name('add_category');
+Route::get('/show_category', [AdminController::class, 'show_category'])->name('show_category');
+Route::get('/update_category/{id}', [AdminController::class, 'updatecategory'])->name('update_category');
+Route::get('/update_subcategory/{id}', [AdminController::class, 'updatesubcategory'])->name('update_subcategory');
+Route::post('/create_category', [AdminController::class, 'create_category'])->name('create_category');
+Route::post('/edit_category/{id}', [AdminController::class, 'edit_category'])->name('edit_category');
+Route::post('/edit_subcategory/{id}', [AdminController::class, 'edit_subcategory'])->name('edit_subcategory');
+Route::get('/add_subcategory', [AdminController::class, 'add_subcategory'])->name('add_subcategory');
+Route::post('/getSubcategories', [AdminController::class, 'getSubcategories'])->name('getSubcategories');
+Route::delete('/subcategory/{id}', [AdminController::class, 'delete_subcategory'])->name('delete_subcategory');
+Route::delete('/category/{id}', [AdminController::class, 'delete_category'])->name('delete_category');
+Route::post('/create_subcategory', [AdminController::class, 'create_subcategory'])->name('create_subcategory');
+Route::get('/show_subcategory', [AdminController::class, 'show_subcategory'])->name('show_subcategory');
--- routes/web.php
+++ routes/web.php
@@ -24,51 +24,17 @@ Route::get('/admin', [AdminController::class, 'index'])->name('home');
 Route::get('/login', [AdminController::class, 'login'])->name('login');
 // Route::get('/register', [AdminController::class, 'register'])->name('register');
 
-Route::get('/add_category', [AdminController::class, 'category'])->name('add_category');
-Route::get('/show_category', [AdminController::class, 'show_category'])->name('show_category');
-Route::get('/update_category/{id}', [AdminController::class, 'updatecategory'])->name('update_category');
-Route::get('/update_subcategory/{id}', [AdminController::class, 'updatesubcategory'])->name('update_subcategory');
-
 Route::get('/download-pdf/{id}', [AdminController::class, 'download-pdf'])->name('download_pdf');
-
-
-Route::post('/create_category', [AdminController::class, 'create_category'])->name('create_category');
-
-Route::post('/edit_category/{id}', [AdminController::class, 'edit_category'])->name('edit_category');
-
-Route::post('/edit_subcategory/{id}', [AdminController::class, 'edit_subcategory'])->name('edit_subcategory');
-
-
-
-
-
-
-
-
-Route::post('/create_subcategory', [AdminController::class, 'create_subcategory'])->name('create_subcategory');
-
-Route::get('/show_subcategory', [AdminController::class, 'show_subcategory'])->name('show_subcategory');
-
-
 
 Route::get('/add_bookings', [AdminController::class, 'add_bookings'])->name('add_bookings');
 Route::get('/show_booking', [AdminController::class, 'show_booking'])->name('show_booking');
 Route::get('/booking_api', [AdminController::class, 'booking_api'])->name('booking_api');
 
 Route::delete('/booking/{id}', [AdminController::class, 'delete_booking'])->name('delete_booking');
-Route::delete('/subcategory/{id}', [AdminController::class, 'delete_subcategory'])->name('delete_subcategory');
-Route::delete('/category/{id}', [AdminController::class, 'delete_category'])->name('delete_category');
 Route::delete('/user/{id}', [AdminController::class, 'delete_user'])->name('delete_user');
 
 
-
-
-
-
 Route::get('/log_out', [AdminController::class, 'log_out'])->name('log_out');
-
-
-Route::post('/getSubcategories', [AdminController::class, 'getSubcategories'])->name('getSubcategories');
 
 Route::post('/getAdditionalInfo', [AdminController::class, 'getAdditionalInfo'])->name('getAdditionalInfo');
 
@@ -82,6 +48,5 @@ Route::post('/login_user', [AdminController::class, 'login_user'])->name('login_
 Route::get('/daterange', 'DateRangeController@index')->name('daterange.index');
 
 Route::post('/add_booking', [AdminController::class, 'add_booking'])->name('add_booking');
-Route::get('/add_subcategory', [AdminController::class, 'add_subcategory'])->name('add_subcategory');
 
-// require __DIR__ . '/category.php';
+require __DIR__ . '/category.php';
